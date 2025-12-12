bd7b6ac27ce21a1bf45df0f67b73d38722373c58
Greg <gregmichaeldavies@gmail.com>
2025-12-12 22:06:16 +0000

    fix: Disabled to avoid type check errors.

--- src/features/surveys/fwx/scoring.ts
+++ src/features/surveys/fwx/scoring.ts
@@ -1,8 +1,8 @@
-import { GetFWXAnswer } from "./types";
+// import { GetFWXAnswer } from "./types";
 
-export const getScores = (getAnswer: GetFWXAnswer) => {
-  const heavy = getAnswer('heavy');
+// export const getScores = (getAnswer: GetFWXAnswer) => {
+//   const heavy = getAnswer('heavy');
   
-  const free = getAnswer('free');
-  const heavier = getAnswer('heavier');
-};
+//   const free = getAnswer('free');
+//   const heavier = getAnswer('heavier');
+// };
